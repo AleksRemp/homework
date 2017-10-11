853290d659d47d3f248ab4aee2644a446f50184d
AleksRemp <rempela@mail.ru>
2017-10-11 03:31:24 +0300

    (new)

--- task_01_03.py
+++ task_01_03.py
@@ -3,16 +3,16 @@ b = int(input())
 c = int(input())
 
 if (a >= b):
-	if (b >= c):
-		print (c,b,a, sep = ',')
-	elif (c >= a):
-	    print(b,a,c, sep = ',')
-	else:
-		print(b,c,a, sep =',')
-else:
     if (b >= c):
-        print(a,c,b, sep = ',')	
-    elif (c <= a):
-       print(c,a,b, sep = ',')
+         print(c, b, a, sep = ', ')
+    elif (c >= a):
+        print(b, a, c, sep = ', ')
     else:
-        print(a,c,b, sep = ',')	
+        print(b, c, a, sep = ', ')
+else:
+    if (c <= a):
+        print(c, a, b, sep = ', ')
+    elif (b <= c):
+        print(a, b, c, sep = ', ')
+    else:
+        print(a, c, b, sep = ', ')
